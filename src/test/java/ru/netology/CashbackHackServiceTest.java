6d28035c278593580c7fb50b4217be83a247f19e
Tereza <ruanapoor@gmail.com>
2023-10-22 16:12:49 +0300

    junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,2 +1,74 @@
-package ru.netology;public class CashbackHackServiceTest {
+package ru.netology;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain0() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemain1() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemain999() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemain1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void testRemain1001() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+
+    }
+    @Test
+    public void testRemain500() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        assertEquals(actual, expected);
+
+    }
+
 }
